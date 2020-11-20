835b8d29514a9cdd674ce33d041c101377cbad3a
Martijn Schendstok <sch.martijn@outlook.com>
2020-11-20 09:51:32 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+File:       main.py
+Author:     AML Project Group 11
+Date:       November 2020
+"""
+
+import sys
+
+
+def main(argv):
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
