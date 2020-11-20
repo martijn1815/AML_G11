0d3c73cc9eb64dd0e0da7c5baab9fd21187734b2
Martijn Schendstok <sch.martijn@outlook.com>
2020-11-20 10:24:00 +0100

    Loads train_lables.csv to a pandas df

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ Date:       November 2020
 """
 
 import sys
+import pandas as pd
 
 
 def main(argv):
-    pass
+    train_labels = pd.read_csv("train_labels.csv")
+    print(train_labels.head())
 
 
 if __name__ == "__main__":
