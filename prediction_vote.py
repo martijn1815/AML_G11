ba9bf475466db1c2dace31292c987c052c666c82
Martijn Schendstok <sch.martijn@outlook.com>
2020-12-08 17:26:54 +0100

    Added overlap for top_k (k>1)

--- prediction_vote.py
+++ prediction_vote.py
@@ -53,15 +53,28 @@ def overlap(dict, x, y):
     count = 0
     for _, value in dict.items():
         if value[x-1] == value[y-1]:
-            count+=1
+            count += 1
     return count/len(dict)
 
 
-def print_overlap(files, dict):
+def overlap2(dict, x, y):
+    count = 0
+    for _, value in dict.items():
+        if value[x-1][0] == value[y-1][0]:
+            count += 1
+    return count/len(dict)
+
+
+def print_overlap(files, dict, top1=True):
     for comb in combinations([i for i in range(1, len(files) + 1)], 2):
-        print("Overlap {0} and {1}: \t {2}".format(files[comb[0]-1],
-                                                   files[comb[1]-1],
-                                                   overlap(dict, comb[0], comb[1])))
+        if top1:
+            print("Overlap {0} and {1}: \t {2}".format(files[comb[0]-1],
+                                                       files[comb[1]-1],
+                                                       overlap(dict, comb[0], comb[1])))
+        else:
+            print("Overlap {0} and {1}: \t {2}".format(files[comb[0]-1],
+                                                       files[comb[1]-1],
+                                                       overlap2(dict, comb[0], comb[1])))
 
 
 def most_frequent(List):
@@ -129,6 +142,7 @@ def main(argv):
         print_overlap(files, predictions_dict)
         write_voted_predictions(predictions_dict, os_system=os_system)
     else:
+        print_overlap(files, predictions_dict, top1=False)
         write_rank_predictions(predictions_dict, top_k, os_system=os_system)
 
 
