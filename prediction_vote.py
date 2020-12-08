f08372d9843f4af8ca349c2f86ec012ce3fa59ea
Martijn Schendstok <sch.martijn@outlook.com>
2020-12-08 22:47:22 +0100

    Ranked predictions using: "predictions_resnet101_org_9_top3", "predictions_augmented_mobilenet_25_top3", "predictions_squeezenet_7_top3"

--- prediction_vote.py
+++ prediction_vote.py
@@ -132,7 +132,8 @@ def write_rank_predictions(dict, top_k, os_system="MacOs"):
 def main(argv):
     os_system = "MacOs"
     # For best results put the files in order of accuracy, from best too worst:
-    files = ["predictions_augmented_mobilenet_25_top3",
+    files = ["predictions_resnet101_org_9_top3",
+             "predictions_augmented_mobilenet_25_top3",
              "predictions_squeezenet_7_top3"]
 
     predictions_dict = load_files(files, os_system=os_system)
