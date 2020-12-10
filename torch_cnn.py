11d9566cfe807bb76e009f9cd50078903d8a2d52
JopRijks <31651172+JopRijks@users.noreply.github.com>
2020-12-10 18:17:37 +0100

    predictiuons alexnnet

--- torch_cnn.py
+++ torch_cnn.py
@@ -442,8 +442,8 @@ def main(argv):
     #model.fc = nn.Linear(2048, 80, bias=True)
 
     # Mobilenet V2:
-    model = models.mobilenet_v2(pretrained=True)
-    model.classifier[1] = nn.Linear(1280, 80, bias=True)
+    # model = models.mobilenet_v2(pretrained=True)
+    # model.classifier[1] = nn.Linear(1280, 80, bias=True)
     # Freeze all layers before the last fully connected layer:
     #for i, child in enumerate(model.features.children()):
     #    if i < 17:
@@ -451,8 +451,8 @@ def main(argv):
     #            param.requires_grad = False
 
     # Alexnet:
-    #model = models.alexnet(pretrained=True)
-    #model.classifier[6] = nn.Linear(4096, 80, bias=True)
+    model = models.alexnet(pretrained=True)
+    model.classifier[6] = nn.Linear(4096, 80, bias=True)
 
     # Mnasnet:
     #model = models.mnasnet1_0(pretrained=True)
@@ -472,14 +472,15 @@ def main(argv):
 
     #print(model)
 
+
     ''' Run model '''
     #pytorch_cnn_train(model, num_epochs=15)
+    model_file = './models/Alexnet/alexnet_fr_aug_14'
     #for i in range(0,15):
     #     model_file = './mobilenetv2_dl2_'+str(i)
     #     print(model_file)
-    #     pytorch_cnn_test(model, model_file=model_file)
-    model_file = './mobilenetv2_dl2_14'
-    pytorch_cnn_classify(model, top_k=1, model_file=model_file, os_systeem="Windows")
+    # pytorch_cnn_test(model, model_file=model_file)
+    pytorch_cnn_classify(model, top_k=5, model_file=model_file, os_systeem="Windows")
 
 
 if __name__ == "__main__":
