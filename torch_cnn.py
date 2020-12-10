40d1563e2fdc3590f6531fe565492f018f27680c
Martijn Schendstok <sch.martijn@outlook.com>
2020-12-10 14:05:20 +0100

    fixed the transform for augmented data

--- torch_cnn.py
+++ torch_cnn.py
@@ -106,7 +106,7 @@ def get_scale_transform(augmented=False):
         scale_transform = transforms.Compose([transforms.ToPILImage(),
                                               transforms.Resize(224),
                                               transforms.CenterCrop(224),
-                                             transforms.RandomHorizontalFlip(p=1),
+                                              transforms.RandomVerticalFlip(p=1),
                                               transforms.ToTensor(),
                                               transforms.Normalize([0.485, 0.456, 0.406],
                                                                    [0.229, 0.224, 0.225])])
