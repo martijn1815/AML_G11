13acf09c8220fd1ac53742ffc1b346fc6a037dc5
Martijn Schendstok <sch.martijn@outlook.com>
2020-12-11 19:50:35 +0100

    Weighted ranking using: "predictions_resnet101_org_9_top3", "predictions_augmented_mobilenet_25_top3", "predictions_mnasnet_25_top3", "predictions_mobilenet_vertflip_11_top3"

--- prediction_vote.py
+++ prediction_vote.py
@@ -155,12 +155,11 @@ def main(argv):
     os_system = "MacOs"
     # For best results (with out weigths) put the files in order of accuracy, from best too worst:
     files = ["predictions_resnet101_org_9_top3",
-             "predictions_resnet101_aug_4_top5",
              "predictions_augmented_mobilenet_25_top3",
-             "predictions_mnasnet_25_top5",
-             "predictions_mobilenet_vertflip_11_top5"]
+             "predictions_mnasnet_25_top3",
+             "predictions_mobilenet_vertflip_11_top3"]
     # Use accuracy as weight:
-    weights = [0.64824, 0.64379, 0.63595, 0.61021, 0.60995]
+    weights = [0.64824, 0.63595, 0.61021, 0.60995]
 
     predictions_dict = load_files(files, os_system=os_system)
     top_k = get_top_k(files)
